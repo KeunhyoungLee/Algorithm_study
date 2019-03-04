000fabaaf7c393d1838a70c2538f0869ec2a16a8
KeunhyoungLee <keunhyoung5@gmail.com>
2019-03-04 21:21:59 +0900

    Update 16235_나무 재테크.cpp

--- BOJ/16235_나무 재테크.cpp
+++ BOJ/16235_나무 재테크.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
-
-/*
-
-*/
 
 int N, M, K; //N:땅크기, M:양분, K:년
 int map[10][10];//양분
@@ -27,20 +22,24 @@ void season() {
 			if (!tree[i][j].empty()) {
 				sort(tree[i][j].begin(), tree[i][j].end());
 
-				vector<int> tree_tmp;
+				vector<int> tree_tmp1;//살아있는 나무
+				vector<int> tree_tmp2;//죽은 나무
 				for (int k = 0; k < tree[i][j].size(); k++)
 					if (map[i][j] >= tree[i][j][k]) {
 						map[i][j] -= tree[i][j][k];
-						tree_tmp.push_back(++tree[i][j][k]);
+						tree_tmp1.push_back(++tree[i][j][k]);
 					}
 					else {
-						map[i][j] += tree[i][j][k] / 2;
+						//map[i][j] += tree[i][j][k] / 2;
 						//tree[i][j].erase(tree[i][j].begin() + k);
 						//k--;
+						tree_tmp2.push_back(tree[i][j][k]);
 					}
 				tree[i][j].clear();
-				for (int k = 0; k < tree_tmp.size(); k++)
-					tree[i][j].push_back(tree_tmp[k]);
+				for (int k = 0; k < tree_tmp1.size(); k++)
+					tree[i][j].push_back(tree_tmp1[k]);
+				for (int k = 0; k < tree_tmp2.size(); k++)
+					map[i][j] += tree_tmp2[k] / 2;
 			}
 	//fall
 	for (int i = 0; i < N; i++)
@@ -48,9 +47,9 @@ void season() {
 			if (!tree[i][j].empty())
 				for (int k = 0; k < tree[i][j].size(); k++)
 					if (tree[i][j][k] % 5 == 0)
-						for (int d = 0; d < 8; i++)
+						for (int d = 0; d < 8; d++)
 							if (i + r[d] >= 0 && i + r[d] < N &&
-								j + c[d] >= 0 && i + r[d] < N)
+								j + c[d] >= 0 && j + r[d] < N)
 								tree[i + r[d]][j + c[d]].push_back(1);
 
 	//winter 양분추가 
