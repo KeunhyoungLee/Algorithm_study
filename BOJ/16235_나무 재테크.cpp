c8973088f9d20baedb6c2020d46dd56af2efafac
KeunhyoungLee <keunhyoung5@gmail.com>
2019-03-04 21:38:19 +0900

    Update 16235_나무 재테크.cpp

--- BOJ/16235_나무 재테크.cpp
+++ BOJ/16235_나무 재테크.cpp
@@ -5,14 +5,10 @@ using namespace std;
 
 int N, M, K; //N:땅크기, M:양분, K:년
 int map[10][10];//양분
-
-//int tree[10][10][1000];//트리 나이
 vector<int> tree[10][10]; //트리좌표,나이
-//vector<int> tree;
-
 int r[8] = { -1,-1,-1,0,0,1,1,1 };
 int c[8] = { -1,0,1,-1,1,-1,0,1 };
-int map_tmp[10][10];
+int map_tmp[10][10];//매년 추가될 양분
 int answer;
 
 void season() {
@@ -24,22 +20,22 @@ void season() {
 
 				vector<int> tree_tmp1;//살아있는 나무
 				vector<int> tree_tmp2;//죽은 나무
+				//살아있는 나무, 죽은 나무 따로 벡터에 담은 후
+				//원래 나무를 clear() 한 다음 살아있는 나무만 원래 나무벡터에 저장
+				//죽은 나무는 2로 나누어 양분으로 더함
 				for (int k = 0; k < tree[i][j].size(); k++)
 					if (map[i][j] >= tree[i][j][k]) {
 						map[i][j] -= tree[i][j][k];
 						tree_tmp1.push_back(++tree[i][j][k]);
 					}
-					else {
-						//map[i][j] += tree[i][j][k] / 2;
-						//tree[i][j].erase(tree[i][j].begin() + k);
-						//k--;
+					else
 						tree_tmp2.push_back(tree[i][j][k]);
-					}
+
 				tree[i][j].clear();
 				for (int k = 0; k < tree_tmp1.size(); k++)
 					tree[i][j].push_back(tree_tmp1[k]);
 				for (int k = 0; k < tree_tmp2.size(); k++)
-					map[i][j] = map[i][j] + tree_tmp2[k] / 2;
+					map[i][j] += tree_tmp2[k] / 2;
 			}
 	//fall
 	for (int i = 0; i < N; i++)
@@ -61,8 +57,6 @@ void season() {
 void print() {
 	for (int i = 0; i < N; i++)
 		for (int j = 0; j < N; j++)
-			//if (!tree[i][j].empty())
-			//	for (int k = 0; k < tree[i][j].size(); k++)
 			answer += tree[i][j].size();
 
 	printf("%d", answer);
